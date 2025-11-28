4cfcabdb33f855d094aba96925b4431f22e6a769
PANKAJ NATH <pyogi8461@gmail.com>
2025-11-28 21:13:18 +0530

    cursor event

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
-var elem = document.querySelectorAll(".element")
-var img= document.querySelectorAll("img")
+addEventListener("mousemove",function(e){
+    console.log(e.clientX,e.clientY);
+    document.body.style.setProperty("--x" , e.x + 'px');
+    document.body.style.setProperty("--y" , e.y + 'px');
 
-// elem.addEventListener('mousemove',function(dets){
-//     img.style.opacity = 1;
-//     img.style.left = dets.x + "px"
-//     img.style.top = dets.y + "px"
-// })
-// elem.addEventListener('mouseleave',function(dets){
-//     img.style.opacity = 0;
-// })
-
-elem.forEach(function(val){
-    val.addEventListener("mouseenter",function(dets){
-        val.childNodes[3].style.opacity = 1;
-    })
-    val.addEventListener("mouseleave",function(){
-        val.childNodes[3].style.opacity = 0;
-    })
-    val.addEventListener("mousemove",function(dets){
-        val.childNodes[3].style.left = dets.x + 'px'
-    })
 })
